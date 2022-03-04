01ecf1720a8f10548712ce5ecaf858f73bde5653
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2022-03-03 22:12:09 -0300

    adicionando comunicação entre os microsserviços com resttemplate

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,6 +2,7 @@ package com.amigoscode.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
@@ -9,14 +10,28 @@ public class CustomerService{
 
 	private final CustomerRepository customerRepository;
 
+	private final RestTemplate restTemplate;
+
 	public void registerCustomer(CustomerRegistrationRequest request) {
 		Customer customer = Customer.builder()
 				.firstName(request.firstName())
 				.lastName(request.lastName())
 				.email(request.email())
 				.build();
-		//TODO validations
+		//TODO check if email valid
+		//TODO check if email not taken
+		customerRepository.saveAndFlush(customer);
 
-		customerRepository.save(customer);
+		FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+				"http://localhost:8081/api/v1/fraud-check/{customerId}",
+				FraudCheckResponse.class,
+				customer.getId()
+		);
+
+		if (fraudCheckResponse.isFraudster()) {
+			throw new IllegalStateException("fraudster");
+		}
+
+		//TODO send notification
 	}
 }
