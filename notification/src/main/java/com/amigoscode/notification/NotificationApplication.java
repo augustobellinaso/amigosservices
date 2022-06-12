3b4c3a6c150554a77d0f3062e51435bcb0d2fc9d
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2022-06-12 14:04:03 -0300

    Testando a publicacao de mensagens na fila

--- notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
+++ notification/src/main/java/com/amigoscode/notification/NotificationApplication.java
@@ -3,10 +3,21 @@ package com.amigoscode.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.amigoscode.notification", "com.amigoscode.amqp"})
 public class NotificationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationApplication.class, args);
 	}
+
+//	@Bean
+//	CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//		return args -> {
+//			producer.publish(new Person("Augusto", 26), notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+//		};
+//	}
+//
+//	record Person(String name, int age) {
+//
+//	}
 }
